f27c4e7190f04d0b9b151276ba834265d5793da6
Svitlana <87758266+Sv0707@users.noreply.github.com>
2021-10-07 13:49:57 +0300

    update

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,6 @@
+
+const fetchCountries = (name) => {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response => response.json());
+} 
+
+export { fetchCountries };
---
+++ src/js/index.js
@@ -0,0 +1,81 @@
+import '/css/styles.css';
+import Notiflix from 'notiflix';
+const debounce = require('lodash.debounce');
+
+import { fetchCountries } from './fetchCountries.js';
+
+const DEBOUNCE_DELAY = 300;
+let markup = '';
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+const makeCountryList = arrChar => {
+  const markup = arrChar
+    .map(
+      item => `<li class="country__item">
+            <img src="${item.flags.svg}" alt="${item.name.common}" width="30" height="30">
+            <span class="country__name">${item.name.common}</span>
+            </li>`,
+    )
+    .join('');
+
+  refs.list.innerHTML = markup;
+};
+
+const makeCountryInfo = item => {
+  const markup = `<p class="country__title"><img src="${item.flags.svg}" alt="${
+    item.name.common
+  }" width="30" height="30">
+  <span class="country__name">${item.name.common}</span></p>
+              <p class="text"><span class="title">Capital:</span> ${item.capital}</p>
+              <p class="text"><span class="title">Population:</span> ${item.population}</p>
+              <p class="text"><span class="title">Languages:</span> ${[...Object.values(item.languages)].join(', ')}</p>`;
+
+  refs.countryInfo.innerHTML = markup;
+};
+
+const onInputClick = () => {
+  const name = refs.input.value.trim();
+  if (name.length === 0) {
+    refs.countryInfo.innerHTML = '';
+    refs.list.innerHTML = '';
+    return;
+  }
+  else {
+    fetchCountries(name)
+    .then(data => {
+      if (data.length > 1 && data.length < 10) {
+        if (refs.countryInfo.textContent.length > 1) {
+          refs.countryInfo.textContent = '';
+        };
+        return makeCountryList(data);
+      }
+
+      if (data.length === 1) {
+        const country = data[0];
+        if (refs.list.textContent.length > 1) {
+          refs.list.textContent = '';
+        };
+        return makeCountryInfo(country);
+      }
+
+      if (data.length > 10) {
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+        return;
+      }
+
+      if (data.status === 404) {
+        Notiflix.Notify.failure("Oops, there is no country with that name.");
+        return;
+      }
+    })
+    .catch(err => console.log(err));
+  }
+
+};
+
+refs.input.addEventListener('input', debounce(onInputClick, DEBOUNCE_DELAY));
